421a00a5279491e8dd991bcc6dbb82ea155d5ff9
li-chang-ping <2902394436@qq.com>
2020-05-21 15:07:30 +0800

    修复缺少 @RequestBody 导致 payment 为 null 的问题
    Signed-off-by: li-chang-ping <2902394436@qq.com>

--- cloud-consumer-order-80/src/main/java/com/lcp/springcloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/lcp/springcloud/controller/OrderController.java
@@ -22,7 +22,7 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @PostMapping("/create")
-    public CommonResult<Payment> create(Payment payment) {
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
