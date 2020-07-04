32ee77595f243610bf4fa50a805238d27cb0f2ba
li-chang-ping <2902394436@qq.com>
2020-07-04 17:10:57 +0800

    nacos服务注册中心基础配置
    Signed-off-by: li-chang-ping <2902394436@qq.com>

---
+++ cloud-alibaba-config-client-3377/src/main/java/com/lcp/springcloud/NacosConfigClientApp3377.java
@@ -0,0 +1,15 @@
+package com.lcp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lcp
+ * @date 2020/7/3 20:56
+ */
+@SpringBootApplication
+public class NacosConfigClientApp3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientApp3377.class, args);
+    }
+}
